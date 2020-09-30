768b9e38c334b80b949edacbed26e6290351a549
Callum Bartlett <callum@voxelfox.co.uk>
2020-10-01 00:56:29 +0100

    Add simp list command

--- cogs/simp_tracker.py
+++ cogs/simp_tracker.py
@@ -17,8 +17,10 @@ class SimpTracker(utils.Cog):
         """Sets you as simping for a user"""
 
         # No pls not yourself
-        if user == ctx.author:
+        if user == ctx.author or user == ctx.guild.me:
             return await ctx.send("Well, _obviously_.")
+        elif user.bot:
+            return await ctx.send("That's just a bit sad, really.")
 
         # Add to db
         async with self.bot.database() as db:
@@ -43,10 +45,16 @@ class SimpTracker(utils.Cog):
         # No pls not yourself
         if user == ctx.author:
             return await ctx.send("Well, _obviously_.")
+        elif user == ctx.guild.me:
+            return await ctx.send("I won't pretend I'm not offended.")
+        elif user.bot:
+            return await ctx.send("Who needs bots, anyway?")
 
         # Add to db
         async with self.bot.database() as db:
-            await db("DELETE FROM simping_users WHERE user_id=$1 AND guild_id=$2 AND simping_for=$3", ctx.author.id, ctx.guild.id, user.id)
+            rows = await db("DELETE FROM simping_users WHERE user_id=$1 AND guild_id=$2 AND simping_for=$3 RETURNING *", ctx.author.id, ctx.guild.id, user.id)
+        if not rows:
+            return await ctx.send(f"You're not simping for {user.mention}!", allowed_mentions=discord.AllowedMentions(users=False))
 
         # Add to cache
         utils.SimpableUser.get_simpable_user(ctx.author.id, ctx.guild.id).remove_simping_for(user.id)
@@ -54,6 +62,32 @@ class SimpTracker(utils.Cog):
 
         # Respond
         await ctx.send(f"You're no longer simping for {user.mention} :<", allowed_mentions=discord.AllowedMentions(users=False))
+
+    @commands.command(cls=utils.Command)
+    @commands.bot_has_permissions(send_messages=True)
+    @commands.guild_only()
+    async def list(self, ctx:utils.Context, user:discord.Member=None):
+        """List who's simping for you"""
+
+        user = user or ctx.author
+        simp_user = utils.SimpableUser.get_simpable_user(user.id, ctx.guild.id)
+        simping_for = [o for o in [ctx.guild.get_member(i.user_id) for i in simp_user.simping_for] if o]
+        being_simped_by = [o for o in [ctx.guild.get_member(i.user_id) for i in simp_user.being_simped_by] if o]
+        mutual_simping = [i for i in simping_for if i in being_simped_by]
+        with utils.Embed(use_random_colour=True) as embed:
+            embed.set_author_to_user(user)
+            if [i for i in simping_for if i not in mutual_simping]:
+                embed.add_field("Simping For", ", ".join([i.mention for i in simping_for if i not in mutual_simping]), inline=False)
+            else:
+                embed.add_field("Simping For", "Nobody... \N{THINKING FACE}")
+            if [i for i in being_simped_by if i not in mutual_simping]:
+                embed.add_field("Being Simped By", ", ".join([i.mention for i in being_simped_by if i not in mutual_simping]), inline=False)
+            else:
+                embed.add_field("Being Simped By", "Nobody... \N{THINKING FACE}")
+            if mutual_simping:
+                embed.add_field("Mutual Simping owo", ", ".join([i.mention for i in mutual_simping]), inline=False)
+        return await ctx.send(embed=embed)
+
 
     @commands.command(cls=utils.Command, aliases=['tree', 't', 'st'])
     @utils.cooldown.cooldown(1, 60, commands.BucketType.member)
@@ -69,42 +103,58 @@ class SimpTracker(utils.Cog):
         # Set up vars for our lines
         lines = []  # lines of dot
         already_added = set()  # user id list
+        added_user_ids = set()
         working = [utils.SimpableUser.get_simpable_user(user.id, ctx.guild.id)]  # people to be looked at
+        working.extend(utils.SimpableUser.get_simpable_user(user.id, ctx.guild.id).simping_for)
+        working.extend(utils.SimpableUser.get_simpable_user(user.id, ctx.guild.id).being_simped_by)
 
         # Go through each valid user
         while working:
 
             # Look at the first user
             current = working[0]
+            added_user_ids.add(current.user_id)
 
             # See if we already added them
             if current.user_id in already_added:
                 working.remove(current)
                 continue
 
-            # See if they're in the server
-            current_member = ctx.guild.get_member(current.user_id)
-            if current_member is None:
-                already_added.add(current.user_id)
-                working.remove(current)
-                continue
+            # # See if they're in the server
+            # current_member = ctx.guild.get_member(current.user_id)
+            # if current_member is None:
+            #     already_added.add(current.user_id)
+            #     working.remove(current)
+            #     continue
 
-            # Add their name
-            current_name = str(current_member).replace('"', '\\"')
-            v = f'{current.user_id}[label="{current_name}"];'
-            if v not in lines:
-                lines.append(v)
+            # # Add their name
+            # current_name = str(current_member).replace('"', '\\"')
+            # v = f'{current.user_id}[label="{current_name}"];'
+            # if v not in lines:
+            #     lines.append(v)
 
             # Add who they simpin for
             for u in current.simping_for:
+                if ctx.guild.get_member(u.user_id) is None:
+                    continue
                 v = f'{current.user_id}->{u.user_id};'
                 if v not in lines:
                     lines.append(v)
+                added_user_ids.add(u.user_id)
+
+            # Add who they simpin by
             for u in current.being_simped_by:
+                if ctx.guild.get_member(u.user_id) is None:
+                    continue
                 v = f'{u.user_id}->{current.user_id};'
                 if v not in lines:
                     lines.append(v)
+                added_user_ids.add(u.user_id)
+
+            # Remove both of those to make it bidirectional
             for u in [i for i in current.simping_for if i in current.being_simped_by]:
+                if ctx.guild.get_member(u.user_id) is None:
+                    continue
                 for x in [f'{current.user_id}->{u.user_id};', f'{u.user_id}->{current.user_id};', f'{current.user_id}->{u.user_id}[dir=both,color=red];', f'{u.user_id}->{current.user_id}[dir=both,color=red];']:
                     try:
                         lines.remove(x)
@@ -114,9 +164,20 @@ class SimpTracker(utils.Cog):
 
             # And add them to the list of users to look at
             already_added.add(current.user_id)
-            working.extend(current.simping_for)
-            working.extend(current.being_simped_by)
+            # working.extend([i for i in current.simping_for if ctx.guild.get_member(i.user_id)])
+            # working.extend([i for i in current.being_simped_by if ctx.guild.get_member(i.user_id)])
             working.remove(current)
+
+        # Remove people who aren't in the server
+        for i in added_user_ids:
+            member = ctx.guild.get_member(i)
+            if member is not None:
+                current_name = str(member).replace('"', '\\"')
+                lines.insert(0, f'{i}[label="{current_name}"];')
+                continue
+            for line in lines:
+                if str(i) in line:
+                    lines.remove(i)
 
         # Now lets output that
         text = "" if len(lines) > 1 else ":c"
